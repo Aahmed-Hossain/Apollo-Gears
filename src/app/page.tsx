6ecd2dad48fa5e9153abd831c94fbe400c0ac023
Aahmed-Hossain <aahmdctec@gmail.com>
2024-04-23 15:52:23 +0600

    integrated nextUi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
     <main className="">
       <h1 className="font-extrabold text-5xl text-yellow-400 text-center">Apollo Gears</h1>
+      <Button>Click me</Button>
     </main>
   );
 }
